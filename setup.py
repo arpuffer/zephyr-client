2f0f11bcb8dcadd2381569737c83d4d737417614
Alex Puffer <alex.puffer@gmail.com>
2020-01-12 09:59:58 -0800

    Added more details to setup.py. Added requirements.txt and function to generate requirements from txt file for setup.  Added version to zephyr.__init__.

--- setup.py
+++ setup.py
@@ -1,18 +1,40 @@
-import setuptools
+"""setup zephyr client library"""
+
+from typing import List
+from setuptools import setup, find_packages
+PKG = 'zephyr'
+VERSION = __import__(PKG).get_version()
+
+def requirements_file_to_list(filename: str = "requirements.txt") -> List:
+    """Create list of requirements from requirements.txt file
+
+    Args:
+        filename (str, optional): . Defaults to "requirements.txt".
+
+    Returns:
+        List
+    """
+    with open(filename, 'r') as infile:
+        return [x.rstrip() for x in list(infile) if x and not x.startswith('#')]
 
 with open("README.md", "r") as fh:
-    long_description = fh.read()
+    LONG_DESCRIPTION = fh.read()
 
-setuptools.setup(
-    name="zephyr-ARPUFFER",
+setup(
+    name=PKG,
     version="0.0.1",
     author="Alex Puffer",
     description="Zephyr for Jira client",
-    long_description=long_description,
+    long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     url="https://github.com/arpuffer/zephyr-client",
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
+    provides=[PKG],
+    install_requires=requirements_file_to_list(),
+    test_suite='tests',
     classifiers=[
+        'Topic :: Internet :: WWW/HTTP',
+        'Intended Audience :: Developers',
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",

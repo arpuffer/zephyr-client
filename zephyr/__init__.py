dc7a90ec17c7d066f0dfc322b5176c887b37a351
Alex Puffer <alex.puffer@gmail.com>
2020-02-23 09:08:03 -0800

    added gitignore

--- zephyr/__init__.py
+++ zephyr/__init__.py
@@ -1,6 +1,15 @@
+'''
+Copyright 2020 Alex Puffer
+Licensed under MIT License
+'''
+
+import logging
 from .zephyr import Zephyr
 
 __version__ = '0.0.1'
+
+LOGGER = logging.getLogger('zephyr')
+LOGGER.setLevel(logging.debug)
 
 def get_version():
     return __version__
